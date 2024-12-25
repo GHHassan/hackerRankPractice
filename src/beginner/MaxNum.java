89fa98d50b0d96451d4e90f0f8a11e00e226b1ef
Hassan <66774893+GHHassan@users.noreply.github.com>
2024-12-25 01:53:43 +0000

    first and second batch of practice

--- src/beginner/MaxNum.java
+++ src/beginner/MaxNum.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class MaxNum {
     public int findMax(List<Integer> nums) {
         int max = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if(nums.get(i) > max) max = nums.get(i);
+        for (Integer num : nums) {
+            if (num > max) max = num;
         }
         return max;
     }
